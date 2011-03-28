04185dbc42f3f421675fccd3d02c6e77356df419
Bauffman <davy@spoon-library.com>
2011-03-28 11:32:03 +0200

    Updated database model.

--- default_www/backend/modules/user_tracker/installer/data/install.sql
+++ default_www/backend/modules/user_tracker/installer/data/install.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS `user_tracker_data` (
-  `id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `value` text COLLATE utf8_unicode_ci NOT NULL,
-  `added_on` datetime NOT NULL
+  `id` varchar(32) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `value` text,
+  `added_on` datetime NOT NULL,
+  KEY `idx_id_name` (`id`,`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `user_tracker_pageviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `visitor_identifier` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `visitor_session` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `url` text COLLATE utf8_unicode_ci NOT NULL,
-  `referrer_host` text COLLATE utf8_unicode_ci,
-  `referrer_path` text COLLATE utf8_unicode_ci,
-  `referrer_query` text COLLATE utf8_unicode_ci,
-  `status` enum('200','404') COLLATE utf8_unicode_ci NOT NULL,
+  `visitor_identifier` varchar(255) NOT NULL,
+  `visitor_session` varchar(255) NOT NULL,
+  `url` text,
+  `referrer_host` text,
+  `referrer_path` text,
+  `referrer_query` text,
+  `status` int(11) NOT NULL,
   `added_on` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
